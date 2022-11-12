f86c6d6572526065b53c41a91dec428a6e47ad6d
Ethyn <etravis@student.cvtc.edu>
2022-11-11 20:34:41 -0600

    Ethyn Changes: Added in code to make the MainActivity.java buttons work to go to their designated locations.

--- app/src/main/java/edu/cvtc/sradke7/contentcreatortcg/MainActivity.java
+++ app/src/main/java/edu/cvtc/sradke7/contentcreatortcg/MainActivity.java
@@ -1,8 +1,13 @@
 package edu.cvtc.sradke7.contentcreatortcg;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+        Button cardViewerButton = (Button)findViewById(R.id.cardViewerBtn);
+        Button ruleBookButton = (Button)findViewById(R.id.rulebookBtn);
+        Button quickStartGuideButton = (Button)findViewById(R.id.quickstartGuideBtn);
+
+        cardViewerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, CardViewer.class));
+            }
+        });
+
+        ruleBookButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, RuleBook.class));
+            }
+        });
+
+        quickStartGuideButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, QuickstartGuide.class));
+            }
+        });
+
+        };
 }
--- app/src/main/java/edu/cvtc/sradke7/contentcreatortcg/RuleBook.java
+++ app/src/main/java/edu/cvtc/sradke7/contentcreatortcg/RuleBook.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class CardViewer extends AppCompatActivity {
+public class RuleBook extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_card_viewer);
+        setContentView(R.layout.activity_rule_book);
     }
 }
