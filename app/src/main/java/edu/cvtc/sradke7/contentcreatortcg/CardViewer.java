c79309f4a9137b410f70ba7b9fae86981eb84c6e
Ethyn <etravis@student.cvtc.edu>
2022-11-15 14:53:29 -0600

    Wired button click to launch website - to be continued.

--- app/src/main/java/edu/cvtc/sradke7/contentcreatortcg/CardViewer.java
+++ app/src/main/java/edu/cvtc/sradke7/contentcreatortcg/CardViewer.java
@@ -2,7 +2,12 @@ package edu.cvtc.sradke7.contentcreatortcg;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class CardViewer extends AppCompatActivity {
 
@@ -10,5 +15,17 @@ public class CardViewer extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_card_viewer);
+
+        Button cardViewBrowser = (Button)findViewById(R.id.viewCardBrowserBtn);
+
+        cardViewBrowser.setOnClickListener(view -> {
+            Intent webpageIntent = new Intent(
+                    Intent.ACTION_VIEW,
+                    Uri.parse("https://scryfall.com")
+            );
+            startActivity(webpageIntent);
+        });
+
+
     }
 }
