eeb27b3beef1592cde5b2f67ea00513db78c41df
Samuel Radke <sradke7@student.cvtc.edu>
2022-11-16 15:55:33 -0600

    background fun

--- app/src/main/java/edu/cvtc/sradke7/contentcreatortcg/CardViewer.java
+++ app/src/main/java/edu/cvtc/sradke7/contentcreatortcg/CardViewer.java
@@ -1,8 +1,10 @@
 package edu.cvtc.sradke7.contentcreatortcg;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
@@ -15,6 +17,12 @@ public class CardViewer extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_card_viewer);
+
+        ConstraintLayout constraintLayout = findViewById(R.id.mainLayout);
+        AnimationDrawable animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(2500);
+        animationDrawable.setExitFadeDuration(5000);
+        animationDrawable.start();
 
         Button cardViewBrowser = (Button)findViewById(R.id.viewCardBrowserBtn);
 
