9843933b5bb9949e0bd3415197220f27b84c1db9
Samuel Radke <sradke7@student.cvtc.edu>
2022-11-29 11:05:01 -0600

    Fixed some navigation.

--- app/src/main/java/edu/cvtc/sradke7/contentcreatortcg/RuleBookTableOfContents.java
+++ app/src/main/java/edu/cvtc/sradke7/contentcreatortcg/RuleBookTableOfContents.java
@@ -9,7 +9,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
-public class RuleBookTableOfContents extends AppCompatActivity implements View.OnClickListener{
+public class RuleBookTableOfContents extends AppCompatActivity implements View.OnClickListener {
     private TextView tvGeneral;
     private Class MyActivityName;
 
